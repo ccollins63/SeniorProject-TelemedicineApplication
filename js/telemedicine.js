a3966c4d70be7e9925866d66fc9fa27cafe1585b
Ella <yourfellaella@gmail.com>
2019-04-07 18:46:40 -0400

    function to hide refill request when btn clicked

---
+++ js/telemedicine.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#accept").click(function(){
+      $("#request").hide();
+    });
+    $("#reject").click(function(){
+      $("#request").hide();
+    });
+  });
