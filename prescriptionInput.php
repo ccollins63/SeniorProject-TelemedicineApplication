336e010f54e8c535047dfbd85ec0e04f49ee7ac2
ccollins63 <ccollins63@github.com>
2019-04-24 16:49:03 -0400

    renaming and added forced login

---
+++ prescriptionInput.php
@@ -0,0 +1,15 @@
+<?php
+    include 'connect-mysql.php';
+    $date = $_POST['bday'];
+    $time = $_POST['time'];
+    $extraNotes = $_POST['extraNotes'];
+    $_SESSION['accountExists'];
+
+
+    $connection = connectDB();
+
+    $sql = "INSERT INTO Prescription (PrescriptionID, PatientID, DoctorID, PrescriptionName, PrescriptionDate, PrescriptionNotes, PrescriptionQuantity) VALUES ('1', '$_SESSION[userID]', '$date', '$time', '', '$extraNotes')";
+
+    $result = $connection->query($sql) or trigger_error($connection->error."[$sql]");
+    header("Location: prescription.php");
+?>
