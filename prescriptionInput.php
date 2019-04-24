45bc5aac5a1e09c68c155fee2020c2feebb47c28
ccollins63 <ccollins63@github.com>
2019-04-24 19:46:11 -0400

    data table

--- prescriptionInput.php
+++ prescriptionInput.php
@@ -1,14 +1,15 @@
 <?php
     include 'connect-mysql.php';
-    $date = $_POST['bday'];
-    $time = $_POST['time'];
-    $extraNotes = $_POST['extraNotes'];
-    $_SESSION['accountExists'];
+    $prescriptionName = $_POST['bday'];
+    $prescriptionDate = $_POST['date'];
+    $prescriptionNotes = $_POST['notes'];
+    $prescriptionQuantity = $_POST['quantity'];
 
+    $_SESSION['accountExists'];
 
     $connection = connectDB();
 
-    $sql = "INSERT INTO Prescription (PrescriptionID, PatientID, DoctorID, PrescriptionName, PrescriptionDate, PrescriptionNotes, PrescriptionQuantity) VALUES ('1', '$_SESSION[userID]', '$date', '$time', '', '$extraNotes')";
+    $sql = "INSERT INTO Prescription (PrescriptionID, PatientID, DoctorID, PrescriptionName, PrescriptionDate, PrescriptionNotes, PrescriptionQuantity) VALUES ('1', '$_SESSION[userID]', '', '$prescriptionName', '$prescriptionDate', '$prescriptionNotes', '$prescriptionQuantity')";
 
     $result = $connection->query($sql) or trigger_error($connection->error."[$sql]");
     header("Location: patientPrescription.php");
