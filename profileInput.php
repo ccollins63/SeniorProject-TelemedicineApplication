84ffd9d18cde885d4e028e1e7e527151c16c9bb3
ccollins63 <ccollins63@github.com>
2019-04-25 01:41:17 -0400

    Update profileInput.php

--- profileInput.php
+++ profileInput.php
@@ -1,16 +1,29 @@
 <?php
     include 'connect-mysql.php';
+
+    $connection = connectDB();
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
     $staticFirstName = $_POST['staticFirstName'];
     $staticLastName = $_POST['staticLastName'];
     $staticEmail = $_POST['staticEmail'];
     $staticBirth = $_POST['staticDateOfBirth'];
 
-    $_SESSION['accountExists'];
 
-    $connection = connectDB();
 
     $sql = "UPDATE User SET FirstName='Bob' WHERE PatientID = 10";
     $result = $connection->query($sql) or trigger_error($connection->error."[$sql]");
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Record uploaded successfully";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 
     header("Location: patientProfile-edit.php");
 ?>
