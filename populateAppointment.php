06ccf35f00c564ebcc29bdfb81b134e2a56793b4
ccollins63 <ccollins63@github.com>
2019-04-24 23:43:03 -0400

    Update populateAppointment.php

--- populateAppointment.php
+++ populateAppointment.php
@@ -8,25 +8,21 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-while ($i < 20)
-{
-    $doctorid = $i + 1;
-    $patientid = $i;
+    $doctorid =  6;
+    $patientid = 4;
     $date = '2019-12-25';
     $time = '00:00:00';
     $doctornotes = 'test';
     $patientnotes = 'test';
 
 // sql to delete a record
-$sql = "INSERT INTO APPOINTMENT (DoctorID, PatientID, Date, Time, DoctorNotes, PatientNotes) VALUES ('$doctorid', '$patientid', '$date', '$time', '$doctornotes', '$patientnotes')";
+$sql = "INSERT INTO Appointment (DoctorID, PatientID, Date, Time, DoctorNotes, PatientNotes) VALUES ('$doctorid', '$patientid', '$date', '$time', '$doctornotes', '$patientnotes')";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "Record deleted successfully";
+    echo "Record uploaded successfully";
 } else {
     echo "Error deleting record: " . mysqli_error($conn);
-}
-$i++;
 }
 
 mysqli_close($conn);
